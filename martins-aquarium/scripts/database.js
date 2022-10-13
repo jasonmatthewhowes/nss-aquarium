8cb1db14b98fd2c90f8317811e8ecfed33532bc1
Jason Howes <110127469+jasonmatthewhowes@users.noreply.github.com>
2022-10-13 09:30:51 -0500

    Add files via upload

---
+++ martins-aquarium/scripts/database.js
@@ -0,0 +1,100 @@
+const fishArray = [
+  {
+    itemID: 1,
+    image:
+      "https://www.liveaquaria.com/images/categories/large/lg80467BicolorBlenny.jpg",
+    species: "Bicolor Blenny",
+    fullName: "BeeBee",
+    length: "3 inches",
+    harvestDate: "44013",
+    diet: "Krill",
+    waterType: "Salt",
+    harvestLocation: "Gulf of Mexico",
+    coordinates:
+      "https://www.google.com/maps/place/21%C2%B018'39.7%22N+89%C2%B040'45.2%22W/@21.3110309,-89.6836903,17z/data=!3m1!4b1!4m9!1m2!2m1!1ssalt+water+smaller+bays!3m5!1s0x0:0x51d737d87901e044!7e2!8m2!3d21.3110263!4d-89.6792106",
+    area: "Progresso",
+    country: "Mexico",
+  },
+  {
+    itemID: 2,
+    image:
+      "https://www.liveaquaria.com/images/categories/large/lg90981PanamicBarnicalBlenny.jpg",
+    species: "Panamic Barnacle Blenny",
+    fullName: "Panama Barry",
+    length: "2 inches",
+    harvestDate: "44745",
+    diet: "Seaweed",
+    waterType: "Salt",
+    harvestLocation: "Pacific Ocean",
+    coordinates:
+      "https://www.google.com/maps/place/23%C2%B019'38.9%22N+110%C2%B011'27.3%22W/@23.3274679,-110.1953983,17z/data=!3m1!4b1!4m9!1m2!2m1!1ssalt+water+smaller+bays!3m5!1s0x0:0x41be2cd00553b9df!7e2!8m2!3d23.3274629!4d-110.1909194",
+    area: "Todos Santos",
+    country: "Mexico",
+  },
+  {
+    itemID: 3,
+    image:
+      "https://www.liveaquaria.com/images/categories/large/lg_Marlbed_Bamboo_Cat_Shark.jpg",
+    species: "Marbled Bamboo Cat Shark",
+    fullName: "Bambie",
+    length: "13 inches",
+    harvestDate: "43586",
+    diet: "Small Fish, humans",
+    waterType: "Salt",
+    harvestLocation: "Chesapeake Bay",
+    coordinates:
+      "https://www.google.com/maps/place/Chesapeake+Bay/@38.2187371,-78.3961895,8z/data=!3m1!4b1!4m8!1m2!2m1!1ssalt+water+smaller+bays!3m4!1s0x89b83db3c325d5e3:0xd677402e2dabc18a!8m2!3d37.5841544!4d-76.1783739",
+    area: "Washington DC",
+    country: "USA",
+  },
+  {
+    itemID: 4,
+    image:
+      "https://www.liveaquaria.com/images/categories/large/lg_76179_Blue_Angel.jpg",
+    species: "Blue Angelfish",
+    fullName: "Angelino",
+    length: "5 inches",
+    harvestDate: "42157",
+    diet: "Algae",
+    waterType: "Fresh",
+    harvestLocation: "Cumberland River",
+    coordinates:
+      "https://www.google.com/maps/place/36%C2%B011'38.2%22N+86%C2%B039'34.5%22W/@36.1939422,-86.6640603,17z/data=!3m1!4b1!4m9!1m2!2m1!1ssalt+water+smaller+bays!3m5!1s0x0:0x29a88955d300bb95!7e2!8m2!3d36.1939378!4d-86.6595813",
+    area: "Nashville",
+    country: "USA",
+  },
+  {
+    itemID: 5,
+    image:
+      "https://www.liveaquaria.com/images/categories/large/lg_89822_Blonde_Tux_Guppy.jpg",
+    species: "Blonde Tux Guppy Group",
+    fullName: "Tuxedo Sam",
+    length: "2 inches",
+    harvestDate: "43586",
+    diet: "Algae",
+    waterType: "Fresh",
+    harvestLocation: "Piercy Priest",
+    coordinates:
+      "https://www.google.com/maps/place/36%C2%B007'49.1%22N+86%C2%B035'21.9%22W/@36.1303182,-86.5939043,17z/data=!3m1!4b1!4m9!1m2!2m1!1ssalt+water+smaller+bays!3m5!1s0x0:0xbd04213a907a9377!7e2!8m2!3d36.1303141!4d-86.5894254",
+    area: "Nashville",
+    country: "USA",
+  },
+  {
+    itemID: 6,
+    image:
+      "https://www.liveaquaria.com/images/categories/large/lg_89727_Ornate_Bichir.jpg",
+    species: "Ornate Bichir",
+    fullName: "SlimJim",
+    length: "8 inches",
+    harvestDate: "42461",
+    diet: "Algae,fish",
+    waterType: "Fresh",
+    harvestLocation: "Lake Arenal",
+    coordinates:
+      "https://www.google.com/maps/place/Lake+Arenal/@10.4918813,-85.1452069,11z/data=!3m1!4b1!4m8!1m2!2m1!1ssalt+water+smaller+bays!3m4!1s0x8fa00445d4aa989b:0x8a2f8115437b0e4d!8m2!3d10.5052242!4d-84.8724284",
+    area: "Lake Arenal",
+    country: "Costa Rica",
+  },
+];
+
+console.log(fishArray[4]);
